b308e3c82e8d61e91e442c1479cc7205f9335238
rivir <rivi.riccardo7@gmail.com>
2022-12-26 18:23:46 +0100

    Functions

--- Function.py
+++ Function.py
@@ -1,7 +1,6 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'#tolgo info e warning
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,mean_absolute_percentage_error,r2_score,mean_absolute_error,make_scorer
 import numpy as np
 from keras.preprocessing.sequence import TimeseriesGenerator
@@ -9,8 +8,10 @@ from sklearn.model_selection import TimeSeriesSplit,cross_validate
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import wilcoxon,ttest_ind
+from sklearn.preprocessing import MinMaxScaler
 import sys
 path=sys.path[1]
+from sklearn.base import TransformerMixin,BaseEstimator
 
 def CheckPath(file):
     if '/' not in file:
@@ -20,10 +21,27 @@ def CheckPath(file):
         isdir = os.path.isdir(path)
         return isdir
 
+class Scaler3D(BaseEstimator,TransformerMixin):
+
+    def __init__(self):
+        self.scaler = MinMaxScaler()
+
+    def fit_transform(self, X, y=None):
+        X=self.scaler.fit_transform(X.reshape(-1, X.shape[-1])).reshape(X.shape)
+        return X
+
+    def fit(self,X,y=None):
+        X=self.scaler.fit(X.reshape(-1, X.shape[-1])).reshape(X.shape)
+        return X
+
+    def transform(self,X):
+        X=self.scaler.transform(X.reshape(-1, X.shape[-1])).reshape(X.shape)
+        return X
+
 def SetArray2DRow(x,y):
     if len(x.shape)==3:
         n_row, time_step, n_feature = x.shape
-        x = x.reshape((n_row * time_step, n_feature))  # a ogni riga associo il valore y, #(12576, 16)
+        x = x.reshape((n_row * time_step, n_feature))
         y = np.repeat(y, repeats=[time_step], axis=0)
         return(x,y)
     else:
@@ -32,8 +50,7 @@ def SetArray2DRow(x,y):
 def SetArray2DCol(x,y):
     if len(x.shape)==3:
         n_row, time_step, n_feature = x.shape
-        x = x.reshape((n_row , n_feature* time_step))  # a ogni riga associo il valore y, #(12576, 16)
-        #y = np.repeat(y, repeats=[time_step], axis=0)
+        x = x.reshape((n_row , n_feature* time_step))
         return(x,y)
     else:
         return "l'array inserito non è 3D"
@@ -42,7 +59,7 @@ def SaveModel(model,model_name):
     if CheckPath(f'{path}/{model_name}/SavedModel'):
         model.save(f'{path}/{model_name}/SavedModel/{model_name}.h5')  # salvo modello
 
-def Evaluation(yu,yp,model_name,file='',fileGlobal='Evaluation/Evaluation'):
+def Evaluation(yu,yp,model_name='',file='',fileGlobal='Evaluation/Evaluation'):
     ev={}
     mse = mean_squared_error(yu, yp)
     ev['MSE']=mse
@@ -64,7 +81,7 @@ def Evaluation(yu,yp,model_name,file='',fileGlobal='Evaluation/Evaluation'):
     else:
         return ev
     #ev = pd.DataFrame(ev.values(), index=ris.keys(), columns=['Evaluation'])
-    if fileGlobal != '' and CheckPath(f'{path}/{file}'):
+    if fileGlobal != '' and model_name!='' and CheckPath(f'{path}/{file}'):
         EvaluationAll(ev, model_name, fileGlobal)
 
 def PredCross(x,models,cv):
@@ -77,7 +94,7 @@ def PredCross(x,models,cv):
     return y_pred
 
 
-def EvaluationCross(model,x,y,model_name,file='',fileCV='',fileGlobal='EvaluationCross/EvaluationCross',fileGlobalCV='EvaluationCross/EvaluationCrossSingle',n_split=5,CV=True):
+def EvaluationCross2(model,x,y,model_name,file='',fileCV='',fileGlobal='EvaluationCross/EvaluationCross',fileGlobalCV='EvaluationCross/EvaluationCrossSingle',n_split=5,CV=True):
     mse = make_scorer(mean_squared_error)
     mae = make_scorer(mean_absolute_error)
     r2 = make_scorer(r2_score)
@@ -128,7 +145,90 @@ def EvaluationCross(model,x,y,model_name,file='',fileCV='',fileGlobal='Evaluatio
     else:
         print(ris)
 
-def EvaluationAllSingle(ev,model_name,fileGlobal='EvaluationCross/EvaluationCrossSingle'):
+def ManualCV(model,x,y,cv,scaler=MinMaxScaler()):
+    ris = {}  # metrics:[metrics cv1,..]
+    for train, test in cv.split(x):
+        x_test=x[test[0]:test[-1]]
+        y_test = y[test[0]:test[-1]]
+
+        x_train=x[train[0]:train[-1]]
+        y_train = y[train[0]:train[-1]]
+
+        x_train = scaler.fit_transform(x_train.reshape(-1, x_train.shape[-1])).reshape(x_train.shape)
+        x_test = scaler.transform(x_test.reshape(-1, x_test.shape[-1])).reshape(x_test.shape)
+        model.fit(x_train,y_train)
+        y_pred=model.predict(x_test)
+
+        ev=Evaluation(y_test,y_pred)
+        for key in ev.index:
+            if key not in ris:
+                ris[key]=[]
+            for val in ev.loc[key].values:
+                ris[key].append(val)
+
+    ris = pd.DataFrame(ris.values(), index=ris.keys(), columns=['CV' + str(i) for i in range(1, len(ris['MSE']) + 1)])
+    return ris
+
+def EvaluationCross(model,x,y,model_name,file='',fileMean='',fileGlobalMean='EvaluationCross/EvaluationCrossMean',fileGlobal='EvaluationCross/EvaluationCross',n_split=5,mean=True,CV_manual=False):
+    mse = make_scorer(mean_squared_error)
+    mae = make_scorer(mean_absolute_error)
+    r2 = make_scorer(r2_score)
+    mape = make_scorer(mean_absolute_percentage_error)
+
+    TimeSplit = TimeSeriesSplit(n_splits=n_split)
+    #PlotSplit(x,TimeSplit)
+    if CV_manual:
+        ris=ManualCV(model,x,y,cv=TimeSplit)
+
+    else:
+        ev = cross_validate(model, x, y, cv=TimeSplit, scoring={'MSE': mse, 'MAE': mae, 'MAPE': mape, 'R2': r2},return_estimator=True)
+
+        ris = {}
+        for key in ev:
+            if 'test' in key:
+                metric = key.split('_')[1].strip()
+                if metric not in ris:
+                    ris[metric]=[]
+                for val in ev[key]:
+                    ris[metric].append(val)
+                if metric == 'MSE':
+                    if 'RMSE' not in ris:
+                        ris['RMSE']=[]
+                    for val in ev[key]:
+                        ris['RMSE'].append(np.sqrt(val))
+        ris = pd.DataFrame(ris.values(), index=ris.keys(), columns=['CV'+str(i) for i in range(1,len(ris['MSE'])+1)])
+
+    EvaluationAllSingle(ris, model_name, fileGlobal)
+
+    if file != '' and CheckPath(f'{path}/{file}'):
+        ris.to_csv(f'{path}/{file}.csv', float_format="%.3f")
+        with open(f'{path}/{file}.txt', 'w') as f:
+            f.write(ris.to_string(header=True, index=True, float_format="%.3f"))
+    else:
+        print(ris)
+
+    if mean==True:
+        ris_mean = {}
+        for key in ris.index:
+            if key not in ris_mean:
+                ris_mean[key] = []
+            ris_mean[key] = sum(ris.loc[key]) / len(ris.loc[key])
+        #print(ris_mean)
+
+        ris_mean = pd.DataFrame(ris_mean.values(), index=ris_mean.keys(), columns=['Evaluation'])
+
+        EvaluationAll(ris_mean, model_name, fileGlobalMean)
+
+        if fileMean != '' and CheckPath(f'{path}/{fileMean}'):
+            ris_mean.to_csv(f'{path}/{fileMean}.csv', float_format="%.3f")
+            with open(f'{path}/{fileMean}.txt', 'w') as f:
+                f.write(ris_mean.to_string(header=True, index=True, float_format="%.3f"))
+        else:
+            print(ris_mean)
+
+
+
+def EvaluationAllSingle(ev,model_name,fileGlobal='EvaluationCross/EvaluationCross'):
     for metric in ev.index:
         ris=ev[ev.index==metric]
         ris=pd.DataFrame(ris.values,columns=ev.columns,index=[f'{model_name}'])
@@ -225,7 +325,6 @@ def SaveGridSearch(param_grid,grid_result,folder=''):#salvo risultati gridsearch
     best = pd.DataFrame(best, index=["Best"])
     #units risulta lista di liste
     for key in d:  # trasformo eventuali liste in stringhe
-        print(d[key])
         for i in range(len(d[key])):
                 if type(d[key][i])==list:
                     d[key][i] = ', '.join(map(str,d[key][i]))
@@ -266,19 +365,19 @@ def ComputeTest(data,test='wilcoxon'):
 
 def Wilcoxon(metrics,file='EvaluationCross/Wilcoxon',diag=True):
     for metric in metrics:
-        data = pd.read_csv(f'{path}/EvaluationCross/EvaluationCrossSingle/{metric}.csv', index_col=[0])
+        data = pd.read_csv(f'{path}/EvaluationCross/EvaluationCross/{metric}.csv', index_col=[0])
         ris=ComputeTest(data,test='wilcoxon')
 
         if file != '' and CheckPath(f'{path}/{file}'): # salva plot
             plt.style.use('seaborn-notebook')
             f, ax = plt.subplots(figsize=(10, 8))
-            plt.title(f'Wilcoxon {metric}', fontdict={'family': 'Arial', 'color': '#001568', 'weight': 'normal', 'size': 20})
+            plt.title(f'{metric}', fontdict={'family': 'Arial', 'color': '#001568', 'weight': 'normal', 'size': 20})
             if diag:
-                ris = ris.drop(columns=ris.columns[-1])
-                mask = np.triu((np.ones_like(ris, dtype=bool)))
-                sns.heatmap(ris[1:], mask=mask[1:], cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True), square=False, ax=ax, annot=True, fmt=".3f")
+                ris1 = ris.drop(columns=ris.columns[-1])
+                mask = np.triu((np.ones_like(ris1, dtype=bool)))
+                sns.heatmap(ris1[1:], mask=mask[1:], cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True), square=False, ax=ax, annot=True, fmt=".3f",annot_kws={"size":35 / np.sqrt(len(ris))})
             else:
-                 sns.heatmap(ris, mask=np.zeros_like(ris, dtype=np.bool), cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True),square=True, ax=ax, annot=True, fmt=".3f")
+                 sns.heatmap(ris, mask=np.zeros_like(ris, dtype=bool), cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True),square=True, ax=ax, annot=True, fmt=".3f",annot_kws={"size":35 / np.sqrt(len(ris))})
 
             ax.set_xticklabels(ax.get_xticklabels(), rotation=0, fontsize=11, fontname='Arial')
             ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=11, fontname='Arial')
@@ -293,19 +392,19 @@ def Wilcoxon(metrics,file='EvaluationCross/Wilcoxon',diag=True):
 
 def Ttest(metrics,file='EvaluationCross/Ttest',diag=True):
     for metric in metrics:
-        data = pd.read_csv(f'{path}/EvaluationCross/EvaluationCrossSingle/{metric}.csv', index_col=[0])
+        data = pd.read_csv(f'{path}/EvaluationCross/EvaluationCross/{metric}.csv', index_col=[0])
         ris=ComputeTest(data,test='Ttest')
 
         if file != '' and CheckPath(f'{path}/{file}'): # salva plot
             plt.style.use('seaborn-notebook')
             f, ax = plt.subplots(figsize=(10, 8))
-            plt.title(f'Ttest {metric}', fontdict={'family': 'Arial', 'color': '#001568', 'weight': 'normal', 'size': 20})
+            plt.title(f'{metric}', fontdict={'family': 'Arial', 'color': '#001568', 'weight': 'normal', 'size': 20})
             if diag:
-                ris = ris.drop(columns=ris.columns[-1])
-                mask = np.triu((np.ones_like(ris, dtype=bool)))
-                sns.heatmap(ris[1:], mask=mask[1:], cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True), square=False, ax=ax, annot=True, fmt=".3f")
+                ris1 = ris.drop(columns=ris.columns[-1])
+                mask = np.triu((np.ones_like(ris1, dtype=bool)))
+                sns.heatmap(ris1[1:], mask=mask[1:], cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True), square=False, ax=ax, annot=True, fmt=".3f",annot_kws={"size":35 / np.sqrt(len(ris))})
             else:
-                 sns.heatmap(ris, mask=np.zeros_like(ris, dtype=np.bool), cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True),square=True, ax=ax, annot=True, fmt=".3f")
+                 sns.heatmap(ris, mask=np.zeros_like(ris, dtype=bool), cmap=sns.light_palette("seagreen", as_cmap=True,reverse=True),square=True, ax=ax, annot=True, fmt=".3f",annot_kws={"size":35 / np.sqrt(len(ris))})
 
             ax.set_xticklabels(ax.get_xticklabels(), rotation=0, fontsize=11, fontname='Arial')
             ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=11, fontname='Arial')
