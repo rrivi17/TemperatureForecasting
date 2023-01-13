5befbceba793b216027dbbb989488d90bfd31cdb
rivir <rivi.riccardo7@gmail.com>
2023-01-13 17:04:20 +0100

    DecisionTree

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,9 +1,8 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from Function import PrepareData,EvaluationCross,SetArray2DCol,Evaluation
-from Plotting import Graph,ResidualPlot,ScatterPlot,PlotTree
+from Plotting import Graph,ResidualPlot,ScatterPlot
 import numpy as np
-
 
 
 if __name__=='__main__':
@@ -11,8 +10,8 @@ if __name__=='__main__':
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0, shuffle=False)
 
-    x_train, y_train = SetArray2DCol(x_train, y_train)
-    x_test, y_test = SetArray2DCol(x_test, y_test)
+    x_train = SetArray2DCol(x_train)
+    x_test= SetArray2DCol(x_test)
 
     model=DecisionTreeRegressor(min_samples_leaf=20,min_samples_split=9,criterion='squared_error',max_depth=20)
 
@@ -23,19 +22,9 @@ if __name__=='__main__':
     ResidualPlot(y_test, y_pred, file="DecisionTree/Graph/DecisionTreeResidualPlot")
     Graph(y_test, y_pred, file="DecisionTree/Graph/DecisionTreeRealVSPred")
     ScatterPlot(x=y_test, y=y_pred, file="DecisionTree/Graph/DecisionTreeScatterPlot")
-    #PlotTree(model,'DecisionTree/Graph/PlotTree')
 
     model = DecisionTreeRegressor(min_samples_leaf=20, min_samples_split=9, criterion='squared_error', max_depth=20)
     x = np.concatenate((x_train, x_test))
     y = np.concatenate((y_train, y_test))
 
     EvaluationCross(model,x,y,model_name='DecisionTree',file="DecisionTree/Evaluation/DecisionTreeEvaluationCross",fileMean="DecisionTree/Evaluation/DecisionTreeEvaluationCrossMean")
-
-
-
-    #PrintTree(model)
-    #tree.plot_tree(model)
-    #fa plot albero
-    #fig=plt.figure()
-    #tree.plot_tree(model, filled=True)#, feature_names=x.columns)
-    #fig.savefig("PlotTree.pdf")
--- DecisionTree/DecisionTreeGridSearch.py
+++ DecisionTree/DecisionTreeGridSearch.py
@@ -1,24 +1,20 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from Function import PrepareData,SetArray2DCol,SaveGridSearch
-from sklearn.model_selection import GridSearchCV,TimeSeriesSplit, RandomizedSearchCV
+from sklearn.model_selection import TimeSeriesSplit, RandomizedSearchCV
 
 def GridSearch(x, y):
     criterion = ['squared_error', 'absolute_error']
     max_depth=[None,10,20,30,50]
-    min_samples_split = list(range(2, 21))# #The minimum number of samples required to split an internal node,default = 2
-    min_samples_leaf=list(range(1, 21))#21
-    # #The minimum number of samples required to be at a leaf node.
-    # A split point at any depth will only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches
+    min_samples_split = list(range(2, 21))
+    min_samples_leaf=list(range(1, 21))
 
-    param_grid={'min_samples_split':min_samples_split,'min_samples_leaf':min_samples_leaf,'max_depth':max_depth,
-                'criterion':criterion}
+    param_grid={'min_samples_split':min_samples_split,'min_samples_leaf':min_samples_leaf,'max_depth':max_depth, 'criterion':criterion}
 
     TimeSplit = TimeSeriesSplit(n_splits=5)
 
     grid = RandomizedSearchCV(estimator=DecisionTreeRegressor(), verbose=1, param_distributions=param_grid, cv=TimeSplit, n_iter=150,scoring='r2')
 
-    #grid = GridSearchCV(estimator=DecisionTreeRegressor(random_state=1), verbose=1, param_grid=param_grid, cv=TimeSplit,scoring='r2')
     grid_result = grid.fit(x, y)
 
     SaveGridSearch(param_grid, grid_result, folder="DecisionTree/GridSearch")
@@ -29,7 +25,7 @@ if __name__=='__main__':
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0, shuffle=False)
 
-    x_train, y_train = SetArray2DCol(x_train, y_train)
-    x_test, y_test = SetArray2DCol(x_test, y_test)
+    x_train = SetArray2DCol(x_train)
+    x_test= SetArray2DCol(x_test)
 
     GridSearch(x_train,y_train)
