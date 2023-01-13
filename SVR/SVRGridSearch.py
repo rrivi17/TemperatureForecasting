0f065ffc1e4e96cc1a337735f7a02b44891b54b9
rivir <rivi.riccardo7@gmail.com>
2023-01-13 16:52:37 +0100

    SVR

--- SVR/SVRGridSearch.py
+++ SVR/SVRGridSearch.py
@@ -1,17 +1,11 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'#tolgo info e warning
 from Function import PrepareData,SetArray2DCol,SaveGridSearch
 from Plotting import PlotSplit
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'#tolgo info e warning
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVR
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import TimeSeriesSplit,RandomizedSearchCV
-import sys
-path=sys.path[1]
 
 
 
@@ -29,8 +23,6 @@ def GridSearch(x, y):
     TimeSplit=TimeSeriesSplit(n_splits=5)
 
 
-    #grid=GridSearchCV(estimator=SVR(), param_grid=param_grid, n_jobs=3, cv=TimeSplit,scoring='r2')
-
     model = Pipeline(steps=[('preprocessing',MinMaxScaler()),('svr',SVR())])
 
     grid=RandomizedSearchCV(estimator=model,random_state=1, param_distributions=param_grid,cv=TimeSplit,n_iter=50,scoring='r2')
@@ -40,18 +32,12 @@ def GridSearch(x, y):
     PlotSplit(x,y,cv=TimeSplit,file='SVR/GridSearch/TimeSerieSplit')
 
 
-
-
 if __name__=='__main__':
     x, y = PrepareData("JenaClimate.csv")
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0, shuffle=False)
 
-    x_train, y_train = SetArray2DCol(x_train, y_train)
-    x_test, y_test = SetArray2DCol(x_test, y_test)
-
-    #scaler = MinMaxScaler()  # si può fare anche dopo manipolazione dati???
-    #x_train = scaler.fit_transform(x_train)
-    #x_test = scaler.transform(x_test)
+    x_train= SetArray2DCol(x_train)
+    x_test = SetArray2DCol(x_test)
 
     GridSearch(x_train,y_train)
